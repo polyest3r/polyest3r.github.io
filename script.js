3aeb5ff88ded4b7b0bafd8637cacd588db33da48
polyest3r | she/her <tdfgfhfh.f8@gmail.com>
2025-06-28 13:55:37 -0500

    dskjfl

--- script.js
+++ script.js
@@ -4,3 +4,18 @@
             overlay.style.display = 'none';
         }, 1000);
     });
+
+    const audio = document.getElementById("audioPlayer");
+const message = document.getElementById("message");
+
+document.addEventListener("click", () => {
+  audio
+    .play()
+    .then(() => {
+      console.log("Audio started playing.");
+      message.style.display = "none";
+    })
+    .catch((error) => {
+      console.error("Playback error:", error);
+    });
+});
